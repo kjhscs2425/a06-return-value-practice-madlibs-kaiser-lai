c4031464adfd49c9948ee6701efbac06f57426a5
kaiser-lai <klai2027@kehillahstudent.org>
2024-11-11 08:42:16 -0800

    done

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-adjective0 = input("Give me an adjective: ")
-noun0 = input("Give me a noun: ")
 
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
-print(story)
+adjective0 = input("Give me an adjective: ")
+noun0 = input("Give me a noun:")
+event = input("give me an event like a party or etc: ")
+   
+other = f"There are many {adjective0} ways to choose a/an {noun0} to read.\
+But there ain't no {event} like a {noun0}"
+print(other)
+
