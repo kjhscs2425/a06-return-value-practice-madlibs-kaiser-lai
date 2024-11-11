d171f742ca95eda70a8a6a0eb1563d23a3e0a0f9
kaiser-lai <klai2027@kehillahstudent.org>
2024-11-11 08:45:52 -0800

    done

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 adjective0 = input("Give me an adjective: ")
 noun0 = input("Give me a noun:")
 event = input("give me an event like a party or etc: ")
-   
+human = input("give me a human")   
+
 other = f"There are many {adjective0} ways to choose a/an {noun0} to read.\
-But there ain't no {event} like a {noun0}"
+But there ain't no {event} like a {human} {event}"
 print(other)
 
